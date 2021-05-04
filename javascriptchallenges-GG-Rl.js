d7ddcfa4c35e57710a8d552c50fdd9209fcb0c1d
Raymond Li <rlii0303@gmail.com>
2021-05-04 16:52:33 -0700

    updated second half

--- javascriptchallenges-GG-Rl.js
+++ javascriptchallenges-GG-Rl.js
@@ -36,15 +36,30 @@
 // Write the code that will log whether theQuestion.length is greater that theAnswer.
 
 //1.
-console.log(34+71)
-console.log(123 - 67)
+// console.log(34+71)
+// console.log(123 - 67)
 
 
-console.log(56 * 23)
-console.log(45/5)
-console.log(5^7)
-console.log(33 %6 )
-console.log('guillermo'.length)
-console.log('guillermo'.includes('e'))
-console.log('guillermo'.charAt())
-console.log('guillermo'.toUpperCase())
+// console.log(56 * 23)
+// console.log(45/5)
+// console.log(5^7)
+// console.log(33 %6 )
+// console.log('guillermo'.length)
+// console.log('guillermo'.includes('e'))
+// console.log('guillermo'.charAt())
+// console.log('guillermo'.toUpperCase())
+// console.log(34 / 3 > 67 / 2);
+// console.log(5 === `5`);
+// console.log(!3 === 3);
+// console.log(`learn`.length === 5 && `student`.length === 7);
+//console.log(`learn`.length === 5 || `student`.length === 10);
+//console.log(`LEARN`.includes(`RN`) );
+//console.log(`LEARN`.includes(`rn`));
+//console.log(`LEARN`[0] === `l`);
+//console.log(`LEARN`[0] === `L`);
+
+var theQuestion = "life, the universe, and everything";
+var theAnswer = 42;
+
+console.log(theAnswer / 2);
+console.log(theQuestion.length);
