41b14c526df774401fbe8b645be59ed2cb5f3ba4
erik-engstrom <eekareeka@yahoo.com>
2021-05-04 16:06:20 -0700

    js challenges 15 completed

--- js-challenges-shazeen-erik.js
+++ js-challenges-shazeen-erik.js
@@ -19,13 +19,17 @@
 // Write the code that will log the length of a string containing your name.
 // console.log("shazeen".length);
 // Write the code that will log whether your string includes the letter "e"?
-console.log("shazeen".includes("e"));
+// console.log("shazeen".includes("e"));
 // Write the code that will log the character at the first index of the string.
+// console.log("shazeen"[0]);
 // Write the code that will log the string in all uppercase letters.
+// console.log("shazeen".toUpperCase());
 // Write the code that will log true or false for the following:
 
 // Is 34 divided by 3 greater than 67 divided by 2?
+// console.log(34/3 > 67/2);
 // Does 5 evaluate to the same as "5"?
+// console.log(5 === "5");
 // Does 5 strictly equal "5"?
 // Does !3 strictly equal 3?
 // Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
