d8d639e0f268343ff0b054e62d6460461f620e65
erik-engstrom <eekareeka@yahoo.com>
2021-05-05 14:36:13 -0700

    conditionals completed thru line 40

--- js-conditionals-diego-erik.js
+++ js-conditionals-diego-erik.js
@@ -7,16 +7,37 @@
 //     console.log("not within budget");
 // }
 // Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-var hungry="yes"
-if(hungry=="yes"){
-    console.log("eat food");
-}
-else{
-    console.log("keep coding");
+// var hungry="yes"
+// if(hungry=="yes"){
+//     console.log("eat food");
+// }
+// else{
+//     console.log("keep coding");
 }
 // Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// var trafficLight = "orange"
+// if(trafficLight === "green"){
+//     console.log("go");
+// }
+// else if(trafficLight === "yellow"){
+//     console.log("slow down");
+// }
+// else{
+//     console.log("STOP");
+// }
 
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
+// var number1 = 10
+// var number2 = 9
+// if(number1 > number2){
+//     console.log(number1);
+// }
+// else if(number2 > number1){
+//     console.log(number2);
+// }
+// else if(number1 === number2){
+//     console.log("numbers are the same");
+// }
 
 // Write an if/else statement that takes a variable of a number and logs whether the number is odd, even, or zero.
 
