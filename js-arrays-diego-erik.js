0b6a25e2bec8b8a8d3950c3a31e367d50f51aa0e
erik-engstrom <eekareeka@yahoo.com>
2021-05-05 16:28:44 -0700

    Arrays challenge thru line 48

--- js-arrays-diego-erik.js
+++ js-arrays-diego-erik.js
@@ -29,21 +29,23 @@ var numbers = [2, 4, 6, 8, 10]
 // console.log(numbers);
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
 var numbers1 = [0]
-console.log(numbers1.concat(numbers))
+// console.log(numbers1.concat(numbers))
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2]
+var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
+// console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
+// console.log(numSet.lastIndexOf(2))
 
 // Write the code that returns the number at the third index.
-
+// console.log(numSet[3])
 // Consider the variable:
 
-// var characters = ["y", "a", "r", "r", "a"]
+var characters = ["y", "a", "r", "r", "a"]
 // Write the code that brings all the letters in the characters array together into a string.
-
+console.log(characters.join(""));
 // Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
 
 // Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
