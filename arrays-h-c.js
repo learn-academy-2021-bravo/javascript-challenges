5d462428bd53697fe6f098a2dcd16517546b01ca
hexflo <hexflores87@yahoo.com>
2021-05-05 14:24:28 -0700

    initial commit

---
+++ arrays-h-c.js
@@ -0,0 +1,12 @@
+var groceryList = ["chips", "dip","cookies"]
+/*console.log(groceryList.push("soda"))
+console.log(groceryList)
+
+console.log(groceryList.push("granola"))
+console.log(groceryList)
+
+console.log(groceryList.slice(0, 2))
+*/
+console.log(groceryList.pop())
+console.log(groceryList.push("beans"))
+console.log(groceryList)
