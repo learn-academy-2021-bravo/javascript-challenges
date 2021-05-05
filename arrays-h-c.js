a94fb1aae04df025e764ec57044153590d74a2ed
csullivan710 <sullyusmc0213@yahoo.com>
2021-05-05 17:18:02 -0500

    Update arrays-h-c.js

--- arrays-h-c.js
+++ arrays-h-c.js
@@ -1,12 +1,33 @@
-var groceryList = ["chips", "dip","cookies"]
-/*console.log(groceryList.push("soda"))
+/*var groceryList = ["chips", "dip","cookies"]
+console.log(groceryList.push("soda"))
 console.log(groceryList)
 
 console.log(groceryList.push("granola"))
 console.log(groceryList)
 
 console.log(groceryList.slice(0, 2))
-*/
+
 console.log(groceryList.pop())
 console.log(groceryList.push("beans"))
 console.log(groceryList)
+*/
+
+/*var numSet = [2, 4, 6, 8, 10]
+console.log(numSet.unshift(0))
+console.log(numSet.push(12))
+console.log(numSet.shift())
+console.log(numSet)
+console.log(numSet.splice(0, 0, 0))
+*/
+
+/*var numSet = [2, 13, 6, 8, 4, 2]
+
+console.log(numSet.indexOf(2))
+console.log(numSet.lastIndexOf(2))
+
+console.log(numSet.slice(3, 4))
+*/
+
+var characters = ["y", "a", "r", "r", "a"]
+
+console.log(characters.join(""))
