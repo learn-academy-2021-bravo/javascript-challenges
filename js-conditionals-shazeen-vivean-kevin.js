0cb31ac77aef24bd411b2504fb5da546ac3a1a03
KevinBurgin <Kevinburgin33@gmail.com>
2021-05-05 14:33:08 -0700

    completed to line 26

--- js-conditionals-shazeen-vivean-kevin.js
+++ js-conditionals-shazeen-vivean-kevin.js
@@ -1,15 +1,28 @@
 // Make sure you try different options and change the variables to ensure properly working code.
 
 // Write an if/else statement that takes a variable of item and logs "in budget" if a price is $100 or less.
-var item = 50
-    if(item<=100) {
-        console.log("In budget")
-    }else {console.log("too much")}
+// var item = 50
+//     if(item<=100) {
+//         console.log("In budget")
+//     }else {console.log("too much")}
 
 // Write an if/else statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
-
+// var hungry =false
+//     if(hungry){
+//         console.log("eat food")
+//     } else{ 
+//         console.log("keep coding")
+//     }
 
 // Write an if/else statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+var trafficlight = "red"
+ if(trafficlight == "green"){
+     console.log("go")
+ } else if(trafficlight == "yellow"){ 
+     console.log("slow down")
+ } else if(trafficlight == "red"){
+     console.log("stop")
+ } else{console.log("error")}
 
 // Write an if/else statement that takes two variables of numbers and outputs the larger number, or logs equal if the numbers are the same.
 
