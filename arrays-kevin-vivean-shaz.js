2dfc8d7975813f0384c94236a7fbfda157cf08c2
Vivean28 <vivean.la@gmail.com>
2021-05-05 16:23:11 -0700

    comleated to line 51

--- arrays-kevin-vivean-shaz.js
+++ arrays-kevin-vivean-shaz.js
@@ -27,19 +27,28 @@ var groceryList = ["chips", "dip", "cookies"]
 
 // Consider the variable:
 
-// var numSet = [2, 13, 6, 8, 4, 2]
+//  var numSet = [2, 13, 6, 8, 4, 2]
 // Write the code that finds the index of the first appearance of the number 2.
+// console.log(numSet[0])
+
+// console.log(numSet.indexOf(2))
 
 // Write the code that finds the index of the last appearance of the number 2.
+// console.log(numSet[5])
 
 // Write the code that returns the number at the third index.
-
+// console.log(numSet[2])
 // Consider the variable:
 
-// var characters = ["y", "a", "r", "r", "a"]
+ var characters = ["y", "a", "r", "r", "a"]
 // Write the code that brings all the letters in the characters array together into a string.
+// characters.join()
+// console.log(characters.join(""))
+// console.log(characters)
 
 // Write the code that reverses the order of the letters in the characters array and saves it into a variable called charsReversed.
+// console.log(characters.reverse())
+
 
 // Write the code that brings all the letters in the charsReversed array together into a string with an asterisk between each letter.
 
