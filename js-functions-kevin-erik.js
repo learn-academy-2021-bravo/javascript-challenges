360eeec8eaa5fc187273c28def5c565d64584ba7
erik-engstrom <eekareeka@yahoo.com>
2021-05-06 15:53:15 -0700

    JS functions almost completed

--- js-functions-kevin-erik.js
+++ js-functions-kevin-erik.js
@@ -34,20 +34,72 @@
 // console.log(multiply(5,6))
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-const divisibleBy = (num1, num2) => {
-    if(num1 % num2 == 0) {
-        return `${num1} is evenly divisible by ${num2}`
-    }
-    else if(num1 % num2 !== 0) {
-        return `${num1} is not evenly divisible by ${num2}`
-    }
-} 
-console.log(divisibleBy(30,10))
+// const divisibleBy = (num1, num2) => {
+//     if(num1 % num2 == 0) {
+//         return `${num1} is evenly divisible by ${num2}`
+//     }
+//     else if(num1 % num2 !== 0) {
+//         return `${num1} is not evenly divisible by ${num2}`
+//     }
+// } 
+// console.log(divisibleBy(30,10))
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+// const assignGrade = (score) =>{
+//     if(score > 90){
+//         return `Your score ${score} is an A`
+//     }
+//     else if(score > 80 || score <89){
+//         return `Your score ${score} is a B`
+//     }
+// }
+// console.log(assignGrade(92))
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
-
+// const isLonger = (str1, str2) => {
+//     if(str1.length > str2.length){
+//         return `${str1} is longer than ${str2}`
+//     }
+//     else if( str1.length < str2.length){
+//         return `${str1} is shorter than ${str2}`
+//     }
+//     else {
+//         return "You done messed up somewhere"
+//     }
+// }
+// console.log(isLonger(8,0))
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+// const greaterNum = (num1, num2) => {
+//         if(num1 > num2){
+//             return `${num1} is bigger`
+//         }
+//         else if( num1 < num2){
+//             return `${num2} is bigger`
+//         }
+//         else {
+//             return "You done messed up somewhere"
+//         }
+//     }
+//     console.log(greaterNum(30,30));
 
 // Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+
+// const yelling = (string) => {
+//     return string.toUpperCase()
+// }
+// console.log(yelling("I'm not yelling, you're yelling!"));
+
+// The World Translator
+// (a) Write a function named helloWorld that takes a language code (e.g. "es", "de", "en") as an argument and returns "Hello World!" in the given language. Ensure you function works for at least 5 languages.
+// (b) Have your function default to returning English.
+
+// The Pluralizer
+// (a) Write a function named pluralizer that takes a number and a singular noun as arguments and returns the number and pluralized form of the noun, if necessary.
+
+// pluralizer(5, "cat")
+// // expected output: "5 cats"
+
+// pluralizer(1, "dog")
+// // expected output: "1 dog"
+// (b) Enhance your function so it can handle a few collective nouns like "sheep", "goose", "child", "person" and "species".
+
