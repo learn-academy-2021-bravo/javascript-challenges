fef3ab7a7e2eb38d35cff2d5c7703173c77149cf
guillermogb88 <guillermo.garcia08@yahoo.com>
2021-05-06 16:04:04 -0700

    completed up to line 67

--- functions-shazeen-guillermo.js
+++ functions-shazeen-guillermo.js
@@ -24,13 +24,46 @@
 // console.log(oddOrEven("twelve"));
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
-
+// const triple = (number) =>{
+//     return number * 3
+// }
+// console.log(triple(2))
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
-
+// const multiply = (number1, number2)=>{
+//     return number1 * number2
+// }
+// console.log(multiply(5,6))
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
-
+// const divisibleBy = (number1, number2)=>{
+//     if(number1 % number2 === 0){
+//         return `${number1} is evenly divisible by ${number2}`
+//     }else{
+//         return `${number1} is not evenly divisble by ${number2}`
+//     }
+// }
+// console.log(divisibleBy(20,10))
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
-
+const assignGrade = (grade)=>{
+    if(grade >=90 && grade <= 100){
+        return "A"
+    }
+    else if(grade>=80 && grade<=89){
+        return "B"
+    }
+    else if (grade >= 70 && grade <= 79){
+        return "C"
+    }
+    else if (grade >= 60 && grade <= 69){
+        return "D"
+    }
+    else if (grade >= 0 && grade <= 59){
+        return "F"
+    }
+    else{
+        return `${grade} is not valid`
+    }
+}
+console.log(assignGrade(67));
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
 
 // Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
