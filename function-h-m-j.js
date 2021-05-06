6f95abeec50fb4f8363de07f224f72c9ea3d3cf4
JJSanch <jon.sanchez003@gmail.com>
2021-05-06 16:18:12 -0700

    changing driver

--- function-h-m-j.js
+++ function-h-m-j.js
@@ -3,7 +3,7 @@
 
 // Write a function named marco that returns "polo".
     // Declare funtion named marco.
-    // Return what's inside polo. 
+    // Return what's inside polo.
     // const marco = () => {
     //     return "polo"
     // }
@@ -14,30 +14,103 @@
 //  Returns name as an arugment.
 // Returns "Welcome, persons name"
     // const greeting = (name) => {
-    //     return `welcome, ${name}` 
+    //     return `welcome, ${name}`
     // }
     // console.log(greeting("hector"))
 // Write a function named oddOrEven that takes a number as an argument and returns whether the number is odd or even.
 // Declare a function named odd or even.
 // Takes a number as an argument
 // Returns the result of the number multiplied by 3.
-    const oddOrEven =(number)=> {
-        if(number%2===0){
-        return `the number ${number} is even`
-     } else{
-         return `the number ${number} is even`
-          } 
-    }
-    console.log(oddOrEven(2))
-    console.log();
+    // const oddOrEven =(number)=> {
+    //     if(number%2===0){
+    //     return `the number ${number} is even`
+    //  } else{
+    //      return `the number ${number} is even`
+    //       }
+    // }
+    // console.log(oddOrEven(2))
+    // console.log();
 
 // Write a function named triple that takes a number as an argument and returns the result of that number multiplied by 3.
-
+  // Write a function that's name is triple
+  // Declare's a number as an argument
+  // Returns the result of a number multiplies by 3
+    // const triple = (number) => {
+    //   return (number * 3)
+    // }
+    // console.log(triple(3));
 // Write a function named multiply that takes two numbers as arguments and returns the result of the numbers multiplied together.
+    // Write a function named multiplied
+    // Create two parameters for the function
+    // returns the result of the two parameters being multiplied
+      // const multiple = (number1 , number2) => {
+      //   return (number1 * number2)
+      // }
+      //   console.log(multiple(2,4));
 
 // Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+    // write a function named divisibleBy
+    // create two parameters for the functions
+    // returns whether the first number is evenly divisible by the second
+    // will log whether first number is evenly divisible by the second
+      // const divisibleBy = (number1 , number2) => {
+      //   if(number1%number2===0){
+      //     return `${number1} is evenly divisible by ${number2}`
+      //   }
+      //   else {
+      //     return `${number1} is not evenly divisible by ${number2}`
+      //   }
+      // }
+      // console.log(divisibleBy(5 , 3));
+      // Write an if/else statement that takes a variable of a grade percentage and logs the letter grade for that percentage,
+      //if the grade is 100% log "perfect score", if the grade is zero log "no grade available."
+      //
+          // var grade = 0 * .100;
+
+          // if(grade === 10){
+          //    console.log("perfect score")
+          // } else if(grade === 9){
+          //     console.log("A-");
+          // } else if(grade === 8){
+          //     console.log("B");
+          // } else if(grade === 7){
+          //     console.log("C");
+          // } else if(grade === 6){
+          //     console.log("D");
+          // } else if(grade === 5){
+          //     console.log("F")
+          // } else {
+          //     console.log("no grade available");
+          // }
+
+
+
 
 // Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+    // write a function named assignGrade
+    // that takes a number score as an arguments
+    // returns the letter grade for the score
+        // const assignGrade = (numberScore) => {
+        //   if(numberScore<=59) {
+        //     return "F"
+        //   }
+        //   else if(numberScore<=69) {
+        //     return "D"
+        //   }
+        //   else if(numberScore<=79) {
+        //     return "C"
+        //   }
+        //   else if(numberScore<=89) {
+        //     return "B"
+        //   }
+        //   else if(numberScore<=100) {
+        //     return "A"
+        //   }
+        //   else {
+        //     return "error"
+        //   }
+        // }
+        //   console.log(assignGrade(95));
 
 // Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
 
